d6d5854b3ca2fae8357d12bfb12d5bba6ee52e58
Scott Allen <71349891+swa34@users.noreply.github.com>
2021-08-03 11:32:57 -0400

    Update rm.c

--- rm.c
+++ rm.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+
+void main(int argc, char* argv[]){
+if(argc!=2 || argv[1]=="--help")
+  {
+    printf("\nusage: rm FileTodelete\n");
+  }
+int status;
+status=remove(argv[1]);
+if(status==0)
+  {
+    printf("successfull\n");
+  }
+else
+   {
+    printf("Unsuccessfull\n");
+   }
+}
 
